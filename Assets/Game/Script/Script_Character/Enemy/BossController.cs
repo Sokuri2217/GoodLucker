dcf5992973415f9db548fe2c2847336a4ad0bee0
YT\Yt <h1202315014@hiratagakuen.onmicrosoft.com>
2025-07-29 12:34:57 +0900

    フォルダ移動

--- Assets/Game/Script/Script_Character/Enemy/BossController.cs
+++ Assets/Game/Script/Script_Character/Enemy/BossController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BossController : EnemyBase
@@ -14,12 +15,40 @@ public class BossController : EnemyBase
     {
         base.Update();
 
+        //変化オブジェクト使用
+        SearchChanger();
+
         //死亡処理
         if (currentHp <= 0) 
         {
             //体力が0以下になったら死亡アニメーションを再生
             animator.SetTrigger("Die");
         }
+    }
+
+    //ステータス変化
+    public bool  SearchChanger()
+    {
+        Vector3 changerDirection = (playerPos.position - transform.position);
+        float distance = changerDirection.magnitude;
+
+        if (distance > searchRange)
+            return false;
+
+        changerDirection.Normalize();
+
+        //視野角チェック
+        float angle = Vector3.Angle(transform.forward, changerDirection);
+        if (angle > viewAngle / 2.0f)
+            return false;
+        //Raycastで視界に入っているかどうか
+        if (!Physics.Raycast(transform.position + Vector3.up * 1.5f, changerDirection, distance, otherLayerMasks))
+        {
+            //プレイヤーを視認している
+            return true;
+        }
+
+        return false;
     }
 
     //削除処理
